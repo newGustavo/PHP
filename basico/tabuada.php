6331a6aa3723962f7536fa548aaf05251a94a36a
newGustavo <gustavonascimento1709@gmail.com>
2023-08-10 16:00:42 -0500

    mais arquivos

--- basico/tabuada.php
+++ basico/tabuada.php
@@ -3,7 +3,7 @@
 for ($i = 1; $i <= 9; $i ++){
     echo PHP_EOL;
     echo "Tabuada do $i" . PHP_EOL;
-    for ($j = 1; $j <= 9; $j++){
+    for ($j = 1; $j <= 10; $j++){
         echo "$i x $j = " . $i * $j . PHP_EOL;
     }
 }
